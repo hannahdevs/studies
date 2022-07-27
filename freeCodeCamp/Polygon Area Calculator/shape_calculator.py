5a9e0622228694070d2999bc911c67f294098025
Hannah <71569469+hannahdevs@users.noreply.github.com>
2022-07-26 21:57:49 -0500

    Completed Project

--- freeCodeCamp/Polygon Area Calculator/shape_calculator.py
+++ freeCodeCamp/Polygon Area Calculator/shape_calculator.py
@@ -1,1 +1,49 @@
-# PROJECT CODE
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:
+            return ( "*" * self.width + "\n" ) * self.height
+
+    def get_amount_inside(self, shape):
+        return int(self.width / shape.width) * int(self.height / shape.height)
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        Rectangle.__init__(self, length, length)
+    
+    def __str__(self):
+        return "Square(side=" + str(self.width) + ")"
+
+    def set_side(self, length):
+        Rectangle.set_width(self, length)
+        Rectangle.set_height(self, length)
+
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_width(self, height):
+        self.set_side(height)
